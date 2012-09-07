8fa762adca039b026c9db4112c3a144ae769f015
Marc Zyngier <Marc.Zyngier@arm.com>
2012-09-07 18:09:58 +0100

    ARM: 7522/1: arch_timers: register a time/cycle counter
    Some subsystems (KVM for example) need access to a cycle counter.
    In the KVM case, this is used to measure the time delta between
    host and guest in order to accurately generate timer events for
    the guest.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    
    Conflicts:
    	arch/arm/include/asm/arch_timer.h

--- arch/arm/kernel/arch_timer.c
+++ arch/arm/kernel/arch_timer.c
@@ -230,6 +230,15 @@ static unsigned long arch_timer_read_current_timer(void)
 	return arch_counter_get_cntpct();
 }
 
+static cycle_t arch_counter_read_cc(const struct cyclecounter *cc)
+{
+	/*
+	 * Always use the physical counter for the clocksource.
+	 * CNTHCTL.PL1PCTEN must be set to 1.
+	 */
+	return arch_counter_get_cntpct();
+}
+
 static struct clocksource clocksource_counter = {
 	.name	= "arch_sys_counter",
 	.rating	= 400,
@@ -237,6 +246,18 @@ static struct clocksource clocksource_counter = {
 	.mask	= CLOCKSOURCE_MASK(56),
 	.flags	= CLOCK_SOURCE_IS_CONTINUOUS,
 };
+
+static struct cyclecounter cyclecounter = {
+	.read	= arch_counter_read_cc,
+	.mask	= CLOCKSOURCE_MASK(56),
+};
+
+static struct timecounter timecounter;
+
+struct timecounter *arch_timer_get_timecounter(void)
+{
+	return &timecounter;
+}
 
 static void __cpuinit arch_timer_stop(struct clock_event_device *clk)
 {
@@ -268,6 +289,10 @@ static int __init arch_timer_common_register(void)
 		return -ENOMEM;
 
 	clocksource_register_hz(&clocksource_counter, arch_timer_rate);
+	cyclecounter.mult = clocksource_counter.mult;
+	cyclecounter.shift = clocksource_counter.shift;
+	timecounter_init(&timecounter, &cyclecounter,
+			 arch_counter_get_cntpct());
 
 	err = request_percpu_irq(arch_timer_ppi, arch_timer_handler,
 				 "arch_timer", arch_timer_evt);
